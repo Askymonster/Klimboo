abe2bb76461b191a6a5c9f7f5b035ad9f1bf5db2
Igor <qaskingq@gmail.com>
2026-02-03 22:03:50 -0300

    Commit att do xml do Samuel OBS: https://www.youtube.com/watch?v=QAKq8UBv4GI veja o video samuel e depois altera as parada baseada nela

---
+++ app/src/main/java/com/example/macaco/tela_dois.kt
@@ -0,0 +1,20 @@
+package com.example.macaco
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class tela_dois : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tela_dois)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
