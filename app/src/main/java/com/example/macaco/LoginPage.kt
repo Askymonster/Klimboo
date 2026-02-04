0f90b166bae305c6a5ae3eda1b904ac61044bbae
Igor <qaskingq@gmail.com>
2026-02-04 20:48:29 -0300

    Att loginpage and registerpage kt

--- app/src/main/java/com/example/macaco/LoginPage.kt
+++ app/src/main/java/com/example/macaco/LoginPage.kt
@@ -1,16 +1,28 @@
 package com.example.macaco
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginPage : AppCompatActivity() {
+    lateinit var editTextEmail: TextInputEditText
+    lateinit var editTextPassword: TextInputEditText
+    lateinit var buttonReg: Button
+    lateinit var FirebaseAuth: mAuth
+    lateinit var ProgressBar: progressBar
+    lateinit var TextView: textView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/macaco/RegisterPage.kt
+++ app/src/main/java/com/example/macaco/RegisterPage.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import org.w3c.dom.Text
 
-class tela_dois : AppCompatActivity() {
+class RegisterPage : AppCompatActivity() {
+    lateinit var editTextEmail: TextInputEditText
+    lateinit var editTextPassword: TextInputEditText
+    lateinit var buttonReg: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tela_dois)
+        setContentView(R.layout.activity_register_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        editTextEmail = findViewById(R.id.email)
+        editTextPassword = findViewById(R.id.password)
+        buttonReg = findViewById(R.id.btn_register)
+
+        buttonReg.
     }
 }
