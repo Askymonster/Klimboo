1da1ec5034d76934de510cce7ec13408b91624b7
Igor <qaskingq@gmail.com>
2026-02-05 16:16:33 -0300

    Register e Login funcionando, precisa polir o código

--- app/src/main/java/com/example/macaco/LoginPage.kt
+++ app/src/main/java/com/example/macaco/LoginPage.kt
@@ -1,24 +1,41 @@
 package com.example.macaco
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginPage : AppCompatActivity() {
+
     lateinit var editTextEmail: TextInputEditText
     lateinit var editTextPassword: TextInputEditText
-    lateinit var buttonReg: Button
-    lateinit var FirebaseAuth: mAuth
-    lateinit var ProgressBar: progressBar
-    lateinit var TextView: textView
+    lateinit var auth: FirebaseAuth
+    lateinit var buttonLogin: Button
+    lateinit var progressBar: ProgressBar
+    lateinit var textView: TextView
 
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(this@LoginPage, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,6 +44,57 @@ class LoginPage : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        auth = FirebaseAuth.getInstance()
+        editTextEmail = findViewById(R.id.email)
+        editTextPassword = findViewById(R.id.password)
+        buttonLogin = findViewById(R.id.btn_login)
+        progressBar = findViewById(R.id.progressBar)
+        textView = findViewById(R.id.registerNow)
+
+        textView.setOnClickListener {
+            val intent = Intent(this, RegisterPage::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        buttonLogin.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if (email.isEmpty()) {
+                Toast.makeText(this@LoginPage, "Insira o E-mail", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                Toast.makeText(this@LoginPage, "Insira a Senha", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        progressBar.visibility = View.GONE
+                        Toast.makeText(
+                            baseContext,
+                            "Conta Criada.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        val intent = Intent(this@LoginPage, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
         }
     }
 }
--- app/src/main/java/com/example/macaco/RegisterPage.kt
+++ app/src/main/java/com/example/macaco/RegisterPage.kt
@@ -1,11 +1,11 @@
 package com.example.macaco
 
-import android.nfc.Tag
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,7 +13,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
-import org.w3c.dom.Text
 
 class RegisterPage : AppCompatActivity() {
     lateinit var editTextEmail: TextInputEditText
@@ -21,6 +20,19 @@ class RegisterPage : AppCompatActivity() {
     lateinit var auth: FirebaseAuth
     lateinit var buttonReg: Button
     lateinit var progressBar: ProgressBar
+    lateinit var textView: TextView
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(this@RegisterPage, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,19 +49,26 @@ class RegisterPage : AppCompatActivity() {
         editTextPassword = findViewById(R.id.password)
         buttonReg = findViewById(R.id.btn_register)
         progressBar = findViewById(R.id.progressBar)
+        textView = findViewById(R.id.loginNow)
+
+        textView.setOnClickListener {
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         buttonReg.setOnClickListener {
             progressBar.visibility = View.VISIBLE
             val email = editTextEmail.text.toString()
-            val password = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
 
             if (email.isEmpty()) {
-                Toast.makeText(this, "Insira o E-mail", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@RegisterPage, "Insira o E-mail", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             if (password.isEmpty()) {
-                Toast.makeText(this, "Insira a Senha", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@RegisterPage, "Insira a Senha", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
@@ -62,6 +81,9 @@ class RegisterPage : AppCompatActivity() {
                             "Conta Criada.",
                             Toast.LENGTH_SHORT,
                         ).show()
+                        val intent = Intent(this, LoginPage::class.java)
+                        startActivity(intent)
+                        finish()
                     } else {
                         // If sign in fails, display a message to the user.
                         Toast.makeText(
