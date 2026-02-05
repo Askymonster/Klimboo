2e9548ef402aad1ecf8ddeb4136274c3763edd55
Igor <qaskingq@gmail.com>
2026-02-04 22:04:40 -0300

    Att RegisterPage.kt, activity_login_page.xml, activity_register_page.xml

--- app/src/main/java/com/example/macaco/RegisterPage.kt
+++ app/src/main/java/com/example/macaco/RegisterPage.kt
@@ -1,17 +1,26 @@
 package com.example.macaco
 
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 import org.w3c.dom.Text
 
 class RegisterPage : AppCompatActivity() {
     lateinit var editTextEmail: TextInputEditText
     lateinit var editTextPassword: TextInputEditText
+    lateinit var auth: FirebaseAuth
     lateinit var buttonReg: Button
+    lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +32,50 @@ class RegisterPage : AppCompatActivity() {
             insets
         }
 
+        auth = FirebaseAuth.getInstance()
         editTextEmail = findViewById(R.id.email)
         editTextPassword = findViewById(R.id.password)
         buttonReg = findViewById(R.id.btn_register)
+        progressBar = findViewById(R.id.progressBar)
 
-        buttonReg.
+        buttonReg.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            val email = editTextEmail.text.toString()
+            val password = editTextEmail.text.toString()
+
+            if (email.isEmpty()) {
+                Toast.makeText(this, "Insira o E-mail", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                Toast.makeText(this, "Insira a Senha", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        progressBar.visibility = View.GONE
+                        Toast.makeText(
+                            baseContext,
+                            "Conta Criada.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            baseContext,
+                            "Autenticação falhou.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+
+
+        }
+
+
+
     }
 }
