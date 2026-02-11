3a34653cc2572543469b4e40f531f7355e375882
Igor <qaskingq@gmail.com>
2026-02-10 22:06:42 -0300

    Att mainpage: imagem engrenagem cliclavel leva a ConfigPage.kt

--- app/src/main/java/com/example/macaco/MainActivity.kt
+++ app/src/main/java/com/example/macaco/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.macaco
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -10,7 +10,7 @@ import com.google.firebase.auth.FirebaseAuth
 class MainActivity : AppCompatActivity() {
 
     lateinit var auth: FirebaseAuth
-    lateinit var textView: TextView
+    lateinit var imageConfig: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +18,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main_page)
 
         auth = FirebaseAuth.getInstance()
-        textView = findViewById(R.id.settings)
+        imageConfig = findViewById(R.id.settings)
 
-        textView.setOnClickListener {
+        imageConfig.setOnClickListener {
             val intent = Intent(this, ConfigPage::class.java)
             startActivity(intent)
             finish()
