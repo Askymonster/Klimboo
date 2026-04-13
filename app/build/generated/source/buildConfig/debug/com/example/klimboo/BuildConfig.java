3bf6df1fbeedd5ea6f34f108887415e837a55ef4
Igor <qaskingq@gmail.com>
2026-04-13 13:08:49 -0300

    final att gitigonre

---
+++ app/build/generated/source/buildConfig/debug/com/example/klimboo/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.klimboo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.klimboo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String SUPABASE_KEY = "sb_publishable_-dAWL0RHtCk4xokE5ANAqA_7VKwR_z9";
+  // Field from default config.
+  public static final String SUPABASE_URL = "https://akufckqkssvegngtuago.supabase.co";
+}
